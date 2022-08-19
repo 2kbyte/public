5e5d4c30a887bef1baae438f866e6361642d7b6f
2kbyte <83149837+2kbyte@users.noreply.github.com>
2022-08-19 18:23:39 -0400

    removed panic key

--- uwuware-lib-fork.lua/default_config.lua
+++ uwuware-lib-fork.lua/default_config.lua
@@ -1,1 +1,1 @@
-{"Menu Background Color":[0,0,0],"UI Background":"Floral","UI Toggle":"BackSlash","Menu Background Color Transparency":1,"Menu Accent Color":[0.7411764860153198,0.5764706134796143,0.9764705896377564],"Panic Key":"F4","Tile Size":90}
+{"Menu Background Color":[0,0,0],"UI Background":"Floral","UI Toggle":"BackSlash","Menu Background Color Transparency":1,"Menu Accent Color":[0.7411764860153198,0.5764706134796143,0.9764705896377564],"Tile Size":90}
